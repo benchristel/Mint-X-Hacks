63891828efc613340a2ce25da4c41c7f28048861
Ben Christel <ben.christel@gmail.com>
2022-05-22 09:05:13 -0700

    Style Nemo desktop icons and add install script

--- README.md
+++ README.md
@@ -11,10 +11,7 @@ Note: I'm not affiliated with Linux Mint, I just use their software.
 
 You'll need the Cinnamon variant of Linux Mint. I've only tested this theme on version 20.3.
 
-Symlink this directory into `~/.themes/`.
-
-To reorder the window titlebar buttons to match Mac OSX, install `dconf-editor`, run it,
-and edit the setting `org.cinnamon.desktop.wm.preferences.button_layout`.
+Clone this repo into `~/.themes/`. Then run the `install.sh` script.
 
 ## Development
 
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+
+# Set button layout to match OSX
+gsettings set org.cinnamon.desktop.wm.preferences button-layout close,minimize,maximize:
+
+# Install global GTK styles. This is needed to style Nemo's desktop items,
+# since Nemo has its own built-in styles that take priority over the theme.
+mkdir -p ~/.config/gtk-3.0
+if ! grep -qs osx-gtk-overrides ~/.config/gtk-3.0/gtk.css; then
+  echo '@import url("osx-gtk-overrides.css");' >> ~/.config/gtk-3.0/gtk.css
+fi
+ln -sf $SCRIPT_DIR/osx-gtk-overrides.css ~/.config/gtk-3.0/osx-gtk-overrides.css
+
+# Install fonts
+tar -xzf fonts.tgz
+mv fonts/* ~/.local/share/fonts
+rm -rf fonts
